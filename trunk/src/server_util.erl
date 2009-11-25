3dc0d1001b6c0224b7c4607509fc61f87555ef64
flier.lu <flier.lu@02687ece-d849-11de-90a4-759a056a3192>
2009-11-25 13:26:25 +0000

    add the MPL license

--- trunk/src/server_util.erl
+++ trunk/src/server_util.erl
@@ -1,6 +1,16 @@
-%% Author: Flier Lu
-%% Created: 2009-11-23
-%% Description: TODO: Add description to server_util
+%%   The contents of this file are subject to the Mozilla Public Licenses
+%%   Version 1.1 (the "License"); you may not use this file except in
+%%   compliance with the License. You may obtain a copy of the License at
+%%   http://www.mozilla.org/MPL/
+%%
+%%   Software distributed under the License is distributed on an "AS IS"
+%%   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+%%   License for the specific language governing rights and limitations
+%%   under the License.
+%%
+%%
+%%   Contributor(s): ______________________________________.
+%%
 -module(server_util).
 
 %%
